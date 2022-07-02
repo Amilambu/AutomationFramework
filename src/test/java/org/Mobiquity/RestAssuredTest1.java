36d83ff0fcd225a2fc208d216b99bee02b05d005
Amilambu <Arlettemilambu@gmail.com>
2022-07-02 14:30:52 +0200

    hi

--- src/test/java/org/Mobiquity/RestAssuredTest1.java
+++ src/test/java/org/Mobiquity/RestAssuredTest1.java
@@ -2,10 +2,22 @@ package org.Mobiquity;
 
 import org.junit.jupiter.api.Test;
 
-public class RestAssuredTest1 {
-  public static void main (String [] args){
 
-      System.out.println("Armelia");
+
+
+public class RestAssuredTest1 {
+
+  @Test
+
+  public static void GetResponseBody() {
+
+    given().
+            when().
+            Get("https://jsonplaceholder.typicode.com/").
+
+            then().
 
   }
+
+  
 }
