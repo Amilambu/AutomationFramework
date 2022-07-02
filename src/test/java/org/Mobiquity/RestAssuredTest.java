7922ba55f3c1675ef277cde850261526e7a0c2ce
Amilambu <Arlettemilambu@gmail.com>
2022-07-03 00:46:34 +0200

    Added 2 more tests

--- src/test/java/org/Mobiquity/RestAssuredTest.java
+++ src/test/java/org/Mobiquity/RestAssuredTest.java
@@ -3,22 +3,45 @@ package org.Mobiquity;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
-
+import static org.hamcrest.Matchers.*;
 public class RestAssuredTest extends Main {
   @Test
-  public  void SearchForUsernameTest() {
+  /**public  void SearchForUsernameTest() {
     searchForUserByUsername();
 
-  }
+  }*/
+
+  /**Search for the user with username “Delphine”.*/
+
   public void searchForUserByUsername(){
     given().
             when()
             .get("https://jsonplaceholder.typicode.com/users")
             .then()
             .statusCode(200)
-            .log()
-            .all();
-    // .body("",);
-  }
+            .body("username", hasItems("Delphine"));
 
+  }
+  /**Use the details fetched to make a search for the posts written by the
+  user.*/
+  @Test
+  public void  searchforpostswrittenbytheuser(){
+    given().
+            when()
+            .get("https://jsonplaceholder.typicode.com/posts")
+            .then()
+            .statusCode(200)
+            .body("id[8]", equalTo(9));
+
+  }
+  /**For each post, fetch the comments and validate if the emails in the
+  comment section are in the proper format.*/
+/**public void validateComments(){
+  given().
+          when()
+          .fetch ("https://jsonplaceholder.typicode.com/posts/1/comments")
+          .then()
+          .statusCode(200)
+          .body("id[8]", equalTo(9));
+}*/
 }
