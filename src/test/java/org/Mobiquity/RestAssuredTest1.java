09ada9dc5e5eadb427208daa64b94f242801acd7
Amilambu <Arlettemilambu@gmail.com>
2022-07-02 08:46:22 +0200

    First project

---
+++ src/test/java/org/Mobiquity/RestAssuredTest1.java
@@ -0,0 +1,11 @@
+package org.Mobiquity;
+
+import org.junit.jupiter.api.Test;
+
+public class RestAssuredTest1 {
+  public static void main (String [] args){
+
+      System.out.println("Armelia");
+
+  }
+}
